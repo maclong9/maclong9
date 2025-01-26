aee4674759b6374e40d4ff2304494413cdcb2adb
Mac Long <maclong9@icloud.com>
2025-01-26 01:47:57 +0000

    chore(setup): clean up and document script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ trap 'cleanup' EXIT
 cleanup() {
 	if [ $? -ne 0 ]; then
 		sudo rm -rf "$HOME"/.config "$HOME"/.gitconfig "$HOME"/.gitignore \
-		"$HOME"/.vim "$HOME"/.vimrc "$HOME"/.zshrc /etc/pam.d/sudo_local
+			"$HOME"/.vim "$HOME"/.vimrc "$HOME"/.zshrc /etc/pam.d/sudo_local
 		(crontab -l 2>/dev/null | sed '$d;$d') | crontab -
 	fi
 }
@@ -14,39 +14,47 @@ cleanup() {
 # Check If Running on macOS
 if [ "$(uname -s)" = "Darwin" ]; then
 	# Enable Touch ID for `sudo`
-	sudo sed 's/^#auth/auth/' /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local > /dev/null
-	
+	sudo sed 's/^#auth/auth/' /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local >/dev/null
+
 	# Install Developer Tools
-	! xcode-select -p >/dev/null 2>&1 && xcode-select --install
-	
+	xcode-select -p >/dev/null 2>&1 && xcode-select --install
+
 	# Accept Developer Tools License
-	! /usr/bin/xcrun clang >/dev/null 2>&1 && sudo xcodebuild -license accept
+	/usr/bin/xcrun clang >/dev/null 2>&1 && sudo xcodebuild -license accept
 fi
 
 # Clone Configuration Files and Symlink to Home Directory
 git clone https://github.com/maclong9/dots .config
 for file in .config/.*; do
 	case "$(basename "$file")" in
-		"." | ".." | ".git") continue ;;
-		*) ln -s "$file" "$HOME/$(basename "$file")" ;;
+	"." | ".." | ".git") continue ;;
+	*) ln -s "$file" "$HOME/$(basename "$file")" ;;
 	esac
 done
 
-# Setup Developer Tooling
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /Users/maclong/.zprofile
-   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/maclong/.zprofile
-   eval "$(/opt/homebrew/bin/brew shellenv)"
-/opt/homebrew/bin/brew install --cask ghostty hammerspoon homerow hyperkey onyx 
-/opt/homebrew/bin/brew install deno helix marksman mas node zoxide && \
-/opt/homebrew/bin/brew install pnpm 
+echo >>/Users/maclong/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>/Users/maclong/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-/opt/homebrew/bin/pnpm setup
-/opt/homebrew/bin/pnpm i -g @tailwindcss/language-server svelte-language-server typescript-language-server vercel turbo vscode-langservers-extracted 
-mas install 1527619437 1662217862
+# Install Tooling and Apps
+HB_PATH="/opt/homebrew/bin"
+"$HB_PATH/brew" install --cask ghostty hammerspoon homerow hyperkey onyx
+"$HB_PATH/brew" install deno helix marksman mas node zoxide
+"$HB_PATH/brew" install pnpm
+"$HB_PATH/mas" install 1527619437 1662217862
+"$HB_PATH/pnpm" setup
+"$HB_PATH/pnpm" i -g @tailwindcss/language-server svelteserver typescript-language-server vscode-langservers-extracted
 
 # Setup Cron Tasks
-(crontab -l 2>/dev/null; echo "0 10 * * * $HOME/.save-the-world.sh") | crontab -
-(crontab -l 2>/dev/null; echo "0 10 * * 0 open /Applications/OnyX.app") | crontab -
+(
+	crontab -l 2>/dev/null
+	echo "0 10 * * * $HOME/.save-the-world.sh"
+) | crontab -
+(
+	crontab -l 2>/dev/null
+	echo "0 10 * * 0 open /Applications/OnyX.app"
+) | crontab -
 
 printf "\033[1;32m✔\033[0m \033[1;37mConfiguration complete\033[0m\n"
